305576f0358c868e95ffeff3e2e4a8a286c99dcd
Indu <ijada@sportz.club>
2019-02-22 18:31:15 +0530

    First Commit

--- app/src/main/java/com/android/samplegitapp/MainActivity.java
+++ app/src/main/java/com/android/samplegitapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Add toast meesage on successful login
+        //Add toast message on successful login
     }
 }
